40055be153452716094e40a0d1012f32091f6c4b
Muhammet Can <muhitosan@gmail.com>
2015-05-06 11:35:54 +0300

    parsing module should gracefully continue if there is a problem while parsing

--- libs/management/commands/parsereddit.py
+++ libs/management/commands/parsereddit.py
@@ -18,7 +18,6 @@ class Command(BaseCommand):
     LIST = "/top/"
     FORMAT = ".json"
     AGENT = "User-Agent: redditlinks:v0.1 (by /u/jeffisabelle)"
-    # PP = pprint.PrettyPrinter(indent=4)
 
     def _create_parse_url(self, subreddit):
         return "%s%s%s%s" % (self.BASE, subreddit, self.LIST, self.FORMAT)
@@ -27,38 +26,37 @@ class Command(BaseCommand):
         subs = Subreddit.objects.all()
         for subreddit in subs:
             print subreddit.title
+            try:
+                headers = {'User-Agent': self.AGENT}
+                url = self._create_parse_url(subreddit)
 
-            headers = {'User-Agent': self.AGENT}
-            url = self._create_parse_url(subreddit)
+                r = requests.get(url, headers=headers)
+                response = json.loads(r.text)
+                response = response["data"]["children"]
 
-            r = requests.get(url, headers=headers)
-            response = json.loads(r.text)
-            response = response["data"]["children"]
+                for item in response[:10]:
+                    item = item["data"]
+                    permalink = self.BASE + item["permalink"]
+                    try:
+                        obj, created = RedditLink.objects.get_or_create(
+                            comments_permalink=permalink, subreddit=subreddit)
 
-            for item in response[:10]:
-                item = item["data"]
-                # self.PP.pprint(item)
-
-                permalink = self.BASE + item["permalink"]
-                try:
-                    obj, created = RedditLink.objects.get_or_create(
-                        comments_permalink=permalink, subreddit=subreddit)
-
-                    if created:
-                        obj.title = item["title"]
-                        obj.url = item["url"]
-                        obj.domain = item["domain"]
-                        obj.score = item["score"]
-                        obj.comments_count = item['num_comments']
-                        obj.author = item['author']
-                    else:
-                        obj.comments_count = item['num_comments']
-                        obj.score = item["score"]
-                    obj.save()
-                except IntegrityError:
-                    print traceback.format_exc()
-
-            time.sleep(2)
+                        if created:
+                            obj.title = item["title"]
+                            obj.url = item["url"]
+                            obj.domain = item["domain"]
+                            obj.score = item["score"]
+                            obj.comments_count = item['num_comments']
+                            obj.author = item['author']
+                        else:
+                            obj.comments_count = item['num_comments']
+                            obj.score = item["score"]
+                        obj.save()
+                    except IntegrityError:
+                        print traceback.format_exc()
+                time.sleep(2)
+            except Exception:
+                pass
 
     def handle(self, *args, **options):
         rate = 'daily' if len(args) == 0 else args[0]
