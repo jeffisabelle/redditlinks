82c0af78273211dc6020b2b483d1c758dff1abcf
Muhammet Can <muhitosan@gmail.com>
2015-05-20 14:49:38 +0300

    update signals

--- members/signals.py
+++ members/signals.py
@@ -1,19 +1,17 @@
 from django.db.models.signals import post_save
-from members.models import MemberSubscription, Member
+from members.models import MemberSubscription
 
 
 def update_subscription_count(sender, **kwargs):
     """
-
-    Arguments:
-    - `sender`:
-    - `**kwargs`:
+    when a new subscription saved for a member,
+    we update the subscription count for the member with
+    this signal.
     """
-
     instance = kwargs.get('instance')
-    instance.member.total_subscription = MemberSubscription.objects.filter(
-        member=instance.member
-    ).count()
+    member = instance.member
+    subs_count = MemberSubscription.objects.filter(member=member).count()
+    instance.member.total_subscription = subs_count
     instance.member.save()
 
 
