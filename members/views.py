353d874e1ec0521abc0857a50e312bb117d4bee4
Muhammet Can <muhitosan@gmail.com>
2015-07-03 00:26:40 +0300

    connect host-name to settings

--- members/views.py
+++ members/views.py
@@ -1,6 +1,7 @@
 from members.models import Member
 from django.views.generic.base import TemplateView, View
 from django.shortcuts import get_object_or_404
+from django.conf import settings
 
 
 class MemberUpdateView(View):
@@ -16,4 +17,5 @@ class ToWeekly(TemplateView, MemberUpdateView):
     def get_context_data(self, **kwargs):
         context = super(ToWeekly, self).get_context_data(**kwargs)
         context['member'] = self.get_member()
+        context['host'] = settings.HOST
         return context
