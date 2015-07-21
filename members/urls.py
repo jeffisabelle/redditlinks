be130fdafefc436ba1775bf6f8e2d834a2079241
Muhammet Can <muhitosan@gmail.com>
2015-07-21 22:17:15 +0300

    add to-weekly and to-daily urls

--- members/urls.py
+++ members/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
-from members.views import ToWeekly
+from members.views import ToWeekly, ToDaily
 
 urlpatterns = patterns(
     '',
     url(r'^toweekly', ToWeekly.as_view(), name='toweekly'),
+    url(r'^todaily', ToDaily.as_view(), name='todaily'),
 )
