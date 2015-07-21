c980758e7266cf9c8d5060037b6851c178a8e29d
Muhammet Can <muhitosan@gmail.com>
2015-07-21 22:17:38 +0300

    subscription rate changers implemented

--- members/views.py
+++ members/views.py
@@ -10,12 +10,35 @@ class MemberUpdateView(View):
         token = self.request.GET['token']
         return get_object_or_404(Member, member_uuid=uuid, member_token=token)
 
+    def update_member_rate(self, member, new_rate):
+        member.rate = new_rate
+        member.save()
+        return member
+
 
 class ToWeekly(TemplateView, MemberUpdateView):
     template_name = 'members/toweekly.html'
 
     def get_context_data(self, **kwargs):
         context = super(ToWeekly, self).get_context_data(**kwargs)
-        context['member'] = self.get_member()
+
+        member = self.get_member()
+        self.update_member_rate(member, "w")
+
+        context['member'] = member
+        context['host'] = settings.HOST
+        return context
+
+
+class ToDaily(TemplateView, MemberUpdateView):
+    template_name = 'members/todaily.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ToDaily, self).get_context_data(**kwargs)
+
+        member = self.get_member()
+        self.update_member_rate(member, "d")
+
+        context['member'] = member
         context['host'] = settings.HOST
         return context
