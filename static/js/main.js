2768c67d642279c6c93caffe209304c287a3ebe4
Muhammet Can <muhitosan@gmail.com>
2015-08-02 21:03:40 +0300

    implement typeahead for subreddit list

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,37 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+var substringMatcher = function(strs) {
+  return function findMatches(q, cb) {
+    var matches, substringRegex;
+
+    // an array that will be populated with substring matches
+    matches = [];
+
+    // regex used to determine if a string contains the substring `q`
+    substrRegex = new RegExp(q, 'i');
+
+    // iterate through the pool of strings and for any string that
+    // contains the substring `q`, add it to the `matches` array
+    $.each(strs, function(i, str) {
+      if (substrRegex.test(str)) {
+        matches.push(str);
+      }
+    });
+
+    cb(matches);
+  };
+};
+
+
+$.get("/subs/subreddits/json/", function(response) {
+    console.log(response);
+    $('.typeahead').typeahead({
+        minLength: 3,
+        highlight: true
+    }, {
+        name: 'my-dataset',
+        source: substringMatcher(response["subreddits"])
+    })
+});
