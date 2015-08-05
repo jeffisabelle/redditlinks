d23a0a5fe28111066687cebbbd83e14ea96c5201
Muhammet Can <muhitosan@gmail.com>
2015-08-05 11:32:55 +0300

    use full path for static subreddits data

--- subs/views.py
+++ subs/views.py
@@ -1,5 +1,6 @@
 import json
 
+from django.conf import settings
 from django.http import JsonResponse
 from django.views.generic.base import View
 
@@ -7,6 +8,9 @@ from django.views.generic.base import View
 class SubredditList(View):
     def get(self, request, *args, **kwargs):
         data = {}
-        with open("static/js/data/subreddits.json") as f:
+        base = settings.BASE_DIR
+        relative = "/static/js/data/subreddits.json"
+        f_path = base + relative
+        with open(f_path, "r") as f:
             data["subreddits"] = json.loads(f.read())
         return JsonResponse(data)
