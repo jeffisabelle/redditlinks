51096ef47ad124a0b59fc173881f37c67cefbe3e
Muhammet Can <mc@muhammetcan.net>
2016-11-06 21:53:52 +0300

    add model inline to see member subscription in the admin

--- members/admin.py
+++ members/admin.py
@@ -11,10 +11,18 @@ class MemberSubscriptionAdmin(admin.ModelAdmin):
     list_display = ('member', 'subscription')
 
 
+class MemberSubscriptionInlineAdmin(admin.TabularInline):
+    model = MemberSubscription
+
+
 class MemberAdmin(admin.ModelAdmin):
     list_display = ('email', 'rate', 'is_active',
                     'total_subscription', 'timezone')
     list_filter = ('rate', 'is_active')
+
+    inlines = [
+        MemberSubscriptionInlineAdmin
+    ]
 
 admin.site.register(Member, MemberAdmin)
 admin.site.register(Subscription, SubscriptionAdmin)
